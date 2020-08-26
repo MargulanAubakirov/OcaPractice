16d878c9f4db0f991df2c50a05cb744d66c4ed04
MargulanAubakirov <nalugram@gmail.com>
2020-08-26 12:09:40 -0500

    do-while

---
+++ src/flowcontrol_chapter5/DemonsrateFor.java
@@ -0,0 +1,13 @@
+package flowcontrol_chapter5;
+
+class DemonstrateFor {
+    public static void main(String args[]) {
+        int ctr = 12;
+        for ( int j=10, k=14;
+              j <= k;
+              ++j, k=k-1, ctr--)
+        {
+            System.out.println(j+":"+k+":"+ctr);
+        }
+    }
+}
--- src/flowcontrol_chapter5/ternary/main.java
+++ src/flowcontrol_chapter5/ternary/main.java
@@ -6,6 +6,8 @@ public class main {
         int qty = 10;
         int discount = (bill > 1000)? (qty > 11)? 10 : 9 : 5;
         System.out.println(discount);
+
+
     }
 }
 
